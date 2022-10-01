9b5e166cae0209e8816347ea768fde409c9332c5
LizRanZim <lrzclass22@gmail.com>
2022-10-01 18:15:15 -0400

    Started to add routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,45 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.hasOne(Category, {
+    foreignKey: 'category_id',
+    // Define an alias for when data is retrieved
+  as: 'products_in_categories'
+});
+
 
 // Categories have many Products
+Category.belongsToMany(Product, {
+  through: {
+  model: Product,
+  uniqueKey: 'category_id',
+  },
+// Define an alias for when data is retrieved
+  as: 'categories_for_products'
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    uniqueKey: 'tag_id',
+  },
+// Define an alias for when data is retrieved
+  as: 'products_with_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    uniqueKey: 'product_id',
+  },
+// Define an alias for when data is retrieved
+  as: 'tags_with_products'
+});
+
+// many to many resource: https://sequelize.org/docs/v6/core-concepts/assocs/
 
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,19 +4,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  // *** Code added by Liz
+  try {
+    const productData = await Product.findAll({
+        include: [{ model: Product, Category, Tag }],
+    });
+    res.status(200).json(productData);
+} catch (err) {
+    res.status(500).json(err);
+}
 });
 
+
+
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  // *** Code added by Liz
+  try {
+    const productData = await Product.findByPk({
+        include: [{ model: Product, Category, Tag }],
+    });
+    res.status(200).json(productData);
+} catch (err) {
+    res.status(500).json(err);
+}
+
 });
 
 // create new product
 router.post('/', (req, res) => {
+
+  // ***Does Liz need to add code here?
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,6 +48,9 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+
+
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
